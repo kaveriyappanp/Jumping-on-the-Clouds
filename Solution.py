3fb7c911d6ed9e457d8d2cb81f05635fca2c747f
Anil Patidar <patidaranil0791@gmail.com>
2018-12-18 00:35:16 +0530

    Create Solution.py

---
+++ Solution.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+// solution function
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c):
+    ans = 0
+    i = 0
+    while i < n - 1:
+        if i + 2 >= n or c[i + 2] == 1:
+            i = i + 1
+            ans = ans + 1
+        else:
+            i = i + 2
+            ans = ans + 1
+    return ans
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
